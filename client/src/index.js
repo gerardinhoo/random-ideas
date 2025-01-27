60389933e025464601490fa531636576c0ad38e6
Gerard Eklu <gerardeklu@Gerards-MacBook-Air.local>
2025-01-26 21:55:08 -0500

    Implemented the ideaList component

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Modal from './components/Modal'
 import './css/style.css';
+import IdeaForm from './components/ideaForm';
 
-const modal = new Modal()
+const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
